35268174a184daea5d94a15cc8415b11c8de5413
Tee <sotomi.tayo@gmail.com>
2023-02-22 19:56:47 -0600

    Debugged

--- country.js
+++ country.js
@@ -1,53 +1,52 @@
 class Country {
-    constructor(name, lang, greeting, colors) {
+    constructor(name, lang, greeting, colors,flag) {
         this.name = name;
         this.lang = lang;
         this.greeting = greeting;
         this.colors = colors;
+        this.flag = flag;
     }
 
     DisplayColors(){
         //where all of the changes happen
         //accessing property
         //this.name  
-          ((c) =>{c=== this.name ;
-            document.getElementById("Color1").innerText = `${c.this.colors[0]}`;
-         document.getElementById("Color2").innerHTML= `${c.this.colors[1]}`;
-       document.getElementById("Color3").innerHTML= `${c.this.colors[2]}`;
-        })         
+        document.querySelector("#countryName").innerText = this.name;
+        document.querySelector("#officialLanguage").innerText = this.lang;
+        document.querySelector("#Helloworld").innerText = this.greeting;
+        document.querySelector("#Color1").style.backgroundColor = this.colors[0];
+        document.querySelector("#Color2").style.backgroundColor = this.colors[1];
+        document.querySelector("#Color3").style.backgroundColor = this.colors[2];
+        document.querySelector("#flag").src=this.flag;
+         
+        }        
         
-      }
-      
-      
-
-      
-    }
-
-let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["red", "white", "blue"]);
-let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"]);
-let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
+      }   
+ 
+let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["red", "white", "blue"],"https://m.media-amazon.com/images/I/615lstmihqL._AC_SX522_.jpg" );
+let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"],"https://cdn11.bigcommerce.com/s-e2nupsxogj/images/stencil/960w/products/9899/29159/elf6afvhex7c2ww75jk1__25540.1675152373.jpg?c=1" );
+let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"],"https://tse2.mm.bing.net/th?id=OIP.uY0eKcElXj5g8p5OebEGVwHaEy&pid=Api&P=0");
 
 
 function SwitchCountry() {
-    selectElement = document.querySelector("#CountryList");
-    let input = selectElement.value; //Replace "USA" with user input
+    let input = document.querySelector("#CountryList").value;
     let country;
 
     if (input === "USA") {
         //set country to usa 
         country = usa;
-       country.DisplayColors();
+       
     }
     else if (input === "Mexico") {
         country = mexico;
-        country.DisplayColors();
+        
 
     }
     else if (input === "Algeria") {
         country = algeria;
-        country.DisplayColors();
+        
     }
-
+    country.DisplayColors();
 }
 
-console.log(this.name[0]);
+
